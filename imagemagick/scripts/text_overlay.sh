38260e23d6570b7b7730d5cb2cfb5db2d289b0b5
Evan <evanmann92@gmail.com>
2025-07-04 22:23:19 -0400

    helvitica

--- imagemagick/scripts/text_overlay.sh
+++ imagemagick/scripts/text_overlay.sh
@@ -9,14 +9,14 @@ text=$(echo "$text" | tr '[:lower:]' '[:upper:]')
 
 # Add black shadow caption first
 /usr/local/bin/convert -size 1820x \
-    -background none -fill black -font "Nimbus-Sans-Bold" -pointsize 210 caption:"$text" \
+    -background none -fill black -font "Helvetica-Bold" -pointsize 210 caption:"$text" \
     miff:- | \
 /usr/local/bin/convert "$input_file" \
     -resize 1920x1080^ -extent 1920x1080 - \
     -gravity northwest -geometry +110+110 -composite \
     miff:- | \
 /usr/local/bin/convert - -size 1820x \
-    -background none -fill "#fdff65" -font "Nimbus-Sans-Bold" -pointsize 210 caption:"$text" \
+    -background none -fill "#fdff65" -font "Helvetica-Bold" -pointsize 210 caption:"$text" \
     -gravity northwest -geometry +100+100 -composite \
     "$output_file" >> "$log" 2>&1
 
