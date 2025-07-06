5f7079b8c9f1525b66c33cc10cbe58f7c63763ce
Evan <evanmann92@gmail.com>
2025-07-06 13:58:06 -0400

    testing pwd and cellauto

--- ffmpeg-convert/scripts/text_overlay.sh
+++ ffmpeg-convert/scripts/text_overlay.sh
@@ -5,30 +5,30 @@ text="$2"
 bgcolor="$3"
 log_file="$4"
 
-title_safe=$(echo "$text" | sed 's/[^a-zA-Z0-9\_\-]/_/g')
+pwd >> "$log_file"
 
-/usr/local/bin/ffmpeg -f lavfi -i cellauto=s=1920x1080:rule=30 -t 10 -c:v libx264 cellauto.mp4 -y
+title_safe=$(echo "$text" | sed 's/[^a-zA-Z0-9\_\-]/_/g')
 
 # 1. Generate cellauto video
 /usr/local/bin/ffmpeg -f lavfi -i cellauto=s=1920x1080:rule=30 -t 10 -c:v libx264 cellauto.mp4 -y
 
 # 2. Generate red color video yellow/gold #F7DC6F pale green (good) #C9E4CA pale blue #C5E3F4 pale red (good) #FFC6C9
-/usr/local/bin/ffmpeg -f lavfi -i color=c=$bgcolor:s=1920x1080 -t 10 -c:v libx264 color.mp4 -y
+# /usr/local/bin/ffmpeg -f lavfi -i color=c=$bgcolor:s=1920x1080 -t 10 -c:v libx264 color.mp4 -y
 
 # 3. Overlay red on top of cellauto and blend with multiply
-/usr/local/bin/ffmpeg -i cellauto.mp4 -i color.mp4 -filter_complex "[0:v] format=rgba [bg]; [1:v] format=rgba [fg]; [bg][fg] blend=all_mode='multiply':all_opacity=1, format=rgba" out.mp4 -y
+# /usr/local/bin/ffmpeg -i cellauto.mp4 -i color.mp4 -filter_complex "[0:v] format=rgba [bg]; [1:v] format=rgba [fg]; [bg][fg] blend=all_mode='multiply':all_opacity=1, format=rgba" out.mp4 -y
 
 # 4. Blur the overlaid video
-/usr/local/bin/ffmpeg -i out.mp4 -vf "gblur=sigma=2" -c:a copy outblur.mp4 -y
+# /usr/local/bin/ffmpeg -i out.mp4 -vf "gblur=sigma=2" -c:a copy outblur.mp4 -y
 
 # 5. Add title slide and output to file with safe filename
-/usr/local/bin/ffmpeg -i outblur.mp4 -vf "drawtext=text='$title_text':x=(w-text_w)/2:y=(h-text_h)/2:fontsize=100:fontcolor=white" -c:a copy "${title_safe//[^a-zA-Z0-9\_\-]/_}.mp4" -y
+# /usr/local/bin/ffmpeg -i outblur.mp4 -vf "drawtext=text='$title_text':x=(w-text_w)/2:y=(h-text_h)/2:fontsize=100:fontcolor=white" -c:a copy "${title_safe//[^a-zA-Z0-9\_\-]/_}.mp4" -y
 
 # Remove temporary files
-rm cellauto.mp4
-rm color.mp4
-rm out.mp4
-rm outblur.mp4
+# rm cellauto.mp4
+# rm color.mp4
+# rm out.mp4
+# rm outblur.mp4
 
 # ffplay "${title_safe//[^a-zA-Z0-9\_\-]/_}.mp4"
 
